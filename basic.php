932b997f6d91e4da827b987f18d624e6055a12cc
root <root@ip-172-31-29-52.us-west-2.compute.internal>
2017-05-15 17:44:27 +0000

    adapted to AWS

--- basic.php
+++ basic.php
@@ -7,4 +7,10 @@ $configv = parse_ini_file("config.ini", false);
 
 $vRelays = parse_ini_file("relays.ini",true);
 
+function url(){
+    $pu = parse_url($_SERVER['REQUEST_URI']);
+    return $pu["scheme"] . "://" . $pu["host"];
+}
+
+
 ?>
